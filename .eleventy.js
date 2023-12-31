4003814cfd24062cbe4891e4c791a0706950b8d8
Ravi <ravipradhan.dev@gmail.com>
2023-12-31 22:05:28 +0000

    GH pages config

--- .eleventy.js
+++ .eleventy.js
@@ -1,27 +1,29 @@
 const minifyHTML = require("./src/transforms/minifyHtml");
 
-const isProduction = process.env.NODE_ENV === "production"
-const resourceDirectory = "src/resources"
+const isProduction = process.env.NODE_ENV === "production";
+const resourceDirectory = "src/resources";
 
 module.exports = (config) => {
-	if (isProduction) {
-		config.addTransform("htmlmin", minifyHTML)
-	}
+  if (isProduction) {
+    config.addTransform("htmlmin", minifyHTML);
+  }
 
-	config.setUseGitIgnore(false)
+  config.setUseGitIgnore(false);
 
-	// Collections
-	config.addCollection("categories", (collection) => require('./src/transforms/buildCategories')(collection, resourceDirectory))
+  // Collections
+  config.addCollection("categories", (collection) =>
+    require("./src/transforms/buildCategories")(collection, resourceDirectory)
+  );
+  config.addPassthroughCopy("src/CNAME");
+  config.addPassthroughCopy("src/resources/**/*.(jpg|jpeg|png)");
 
-	config.addPassthroughCopy("src/resources/**/*.(jpg|jpeg|png)")
-
-	return {
-		markdownTemplateEngine: "njk",
-		dataTemplateEngine: "njk",
-		htmlTemplateEngine: "njk",
-		dir: {
-			input: "src",
-			output: "dist"
-		}
-	}
-}
+  return {
+    markdownTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dir: {
+      input: "src",
+      output: "dist",
+    },
+  };
+};
